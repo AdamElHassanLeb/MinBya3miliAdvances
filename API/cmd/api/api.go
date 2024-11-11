6983769a16fbb92dbf65a55f4ec4149bad73cec8
Adam ElHassan <Adoomeh21@gmail.com>
2024-11-11 12:35:11 +0200

    /api in endpoint

--- API/cmd/api/api.go
+++ API/cmd/api/api.go
@@ -35,30 +35,32 @@ func (app *application) mount() http.Handler {
 
 	r.Use(middleware.Timeout(60 * time.Second))
 
-	r.Route("/v1", func(mainRouter chi.Router) {
-		mainRouter.Route("/user", func(userRouter chi.Router) {
-			userRouter.Get("/users", app.GetAllUsers)
-			userRouter.Get("/userId/{id}", app.GetUserById)
-			userRouter.Get("/userName/{name}", app.GetUserByName)
-			userRouter.Post("/create", app.CreateUser)
-			userRouter.With(Middleware.AuthMiddleware).Delete("/delete/{id}", app.DeleteUser)
-			userRouter.With(Middleware.AuthMiddleware).Put("/update/{id}", app.UpdateUser)
-			userRouter.Get("/auth", app.authUser)
-		})
-		mainRouter.Route("/listing", func(listingRouter chi.Router) {
-			listingRouter.Get("/listings/{type}", app.GetAllListings)
-			listingRouter.Get("/listingId/{id}", app.GetListingByID)
-			listingRouter.Get("/listings/user/{user_id}/{type}", app.GetListingsByUserID)
-			listingRouter.Get("/search/{query}/{type}", app.GetListingsBySearch)
-			listingRouter.Get("/date/{type}", app.GetListingsByDate)
-			listingRouter.Get("/date/search/{query}/{type}", app.GetListingsByDateAndSearch)
-			listingRouter.Get("/distance/{latitude}/{longitude}/{max_distance}/{type}", app.GetListingsByDistance)
-			listingRouter.Get("/location/{latitude}/{longitude}/{max_range}/{type}", app.GetListingsByLocation)
-			listingRouter.Post("/create", app.CreateListing)
-			listingRouter.Put("/update/{id}", app.UpdateListing)
-			listingRouter.Delete("/delete/{id}", app.DeleteListing)
-		})
+	r.Route("/api", func(v1Router chi.Router) {
+		v1Router.Route("/v1", func(mainRouter chi.Router) {
+			mainRouter.Route("/user", func(userRouter chi.Router) {
+				userRouter.Get("/users", app.GetAllUsers)
+				userRouter.Get("/userId/{id}", app.GetUserById)
+				userRouter.Get("/userName/{name}", app.GetUserByName)
+				userRouter.Post("/create", app.CreateUser)
+				userRouter.With(Middleware.AuthMiddleware).Delete("/delete/{id}", app.DeleteUser)
+				userRouter.With(Middleware.AuthMiddleware).Put("/update/{id}", app.UpdateUser)
+				userRouter.Get("/auth", app.authUser)
+			})
+			mainRouter.Route("/listing", func(listingRouter chi.Router) {
+				listingRouter.Get("/listings/{type}", app.GetAllListings)
+				listingRouter.Get("/listingId/{id}", app.GetListingByID)
+				listingRouter.Get("/listings/user/{user_id}/{type}", app.GetListingsByUserID)
+				listingRouter.Get("/search/{query}/{type}", app.GetListingsBySearch)
+				listingRouter.Get("/date/{type}", app.GetListingsByDate)
+				listingRouter.Get("/date/search/{query}/{type}", app.GetListingsByDateAndSearch)
+				listingRouter.Get("/distance/{latitude}/{longitude}/{max_distance}/{type}", app.GetListingsByDistance)
+				listingRouter.Get("/location/{latitude}/{longitude}/{max_range}/{type}", app.GetListingsByLocation)
+				listingRouter.Post("/create", app.CreateListing)
+				listingRouter.Put("/update/{id}", app.UpdateListing)
+				listingRouter.Delete("/delete/{id}", app.DeleteListing)
+			})
 
+		})
 	})
 
 	return r
